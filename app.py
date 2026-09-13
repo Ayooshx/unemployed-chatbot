c3b89b02ff5ef540ca6f51c913f7d5d4e4536e02
ayooshx <mondalayush023@gmail.com>
2026-09-13 10:38:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ except KeyError:
     st.stop()
 
 client = Groq(api_key=GROQ_API_KEY)
-MODEL_ID = "llama-3.1-8b-instant" 
+MODEL_ID = "llama3-8b-8192" 
 
 st.set_page_config(page_title="Unemployed AI", page_icon="😭💸")
 
